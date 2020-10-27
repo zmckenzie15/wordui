2ed8bc61cd429d43842c9dcb562a57bd85a7d226
Ellen Spertus <spertus@mills.edu>
2020-10-27 13:16:52 -0700

    initial commit

--- src/main/java/edu/mills/cs180a/wordui/FXMLController.java
+++ src/main/java/edu/mills/cs180a/wordui/FXMLController.java
@@ -2,11 +2,8 @@ package edu.mills.cs180a.wordui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import edu.mills.cs180a.wordui.model.SampleData;
 import edu.mills.cs180a.wordui.model.WordRecord;
-import edu.mills.cs180a.wordui.model.WordRecord.SortOrder;
-import javafx.beans.Observable;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
@@ -18,150 +15,120 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
-import javafx.util.Callback;
 
 public class FXMLController implements Initializable {
-	@FXML
-	private ChoiceBox<WordRecord.SortOrder> sortChoiceBox;
-	@FXML
-	private TextField wordTextField;
-	@FXML
-	private TextField frequencyTextField;
-	@FXML
-	private TextArea definitionTextArea;
-	@FXML
-	private Button removeButton;
-	@FXML
-	private Button createButton;
-	@FXML
-	private Button updateButton;
-	@FXML
-	private ListView<WordRecord> listView;
+    @FXML
+    private TextField wordTextField;
+    @FXML
+    private TextField frequencyTextField;
+    @FXML
+    private TextArea definitionTextArea;
+    @FXML
+    private Button removeButton;
+    @FXML
+    private Button createButton;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private ListView<WordRecord> listView;
 
-	public static Callback<WordRecord, Observable[]> extractor = wr -> new Observable[] { wr.definitionProperty(),
-			wr.frequencyProperty() };
+    private final ObservableList<WordRecord> wordRecordList = FXCollections.observableArrayList();
 
-	private final ObservableList<WordRecord> wordRecordList = FXCollections.observableArrayList(extractor);
+    private WordRecord selectedWordRecord;
+    private final BooleanProperty modifiedProperty = new SimpleBooleanProperty(false);
+    private ChangeListener<WordRecord> wordRecordChangeListener = new WordRecordChangeListener();
 
-	private WordRecord selectedWordRecord;
-	private final BooleanProperty modifiedProperty = new SimpleBooleanProperty(false);
-	private ChangeListener<WordRecord> wordRecordChangeListener = new WordRecordChangeListener();
+    private class WordRecordChangeListener implements ChangeListener<WordRecord> {
+        @Override
+        public void changed(ObservableValue<? extends WordRecord> observable, WordRecord oldValue,
+                WordRecord newValue) {
+            // newValue can be null if nothing is selected.
+            System.out.println("Selected item: " + newValue);
+            selectedWordRecord = newValue;
+            modifiedProperty.set(false);
+            if (newValue != null) {
+                wordTextField.setText(selectedWordRecord.getWord());
+                definitionTextArea.setText(selectedWordRecord.getDefinition());
+            } else {
+                wordTextField.setText("");
+                definitionTextArea.setText("");
+            }
+        }
+    }
 
-	private class WordRecordChangeListener implements ChangeListener<WordRecord> {
-		@Override
-		public void changed(ObservableValue<? extends WordRecord> observable, WordRecord oldValue,
-				WordRecord newValue) {
-			System.out.println("Selected item: " + newValue);
-			// newValue can be null if nothing is selected
-			selectedWordRecord = newValue;
-			modifiedProperty.set(false);
-			if (newValue != null) {
-				wordTextField.setText(selectedWordRecord.getWord());
-				frequencyTextField.setText(String.valueOf(selectedWordRecord.getFrequency()));
-				definitionTextArea.setText(selectedWordRecord.getDefinition());
-			} else {
-				wordTextField.setText("");
-				frequencyTextField.setText("");
-				definitionTextArea.setText("");
-			}
-		}
-	}
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Initialize the list.
+        SampleData.fillSampleData(wordRecordList);
 
-	private void setSortOrder(SortOrder newValue) {
-		SortedList<WordRecord> sortedList = new SortedList<>(wordRecordList);
-		sortedList.setComparator(newValue.getComparator());
-		listView.setItems(sortedList);
-	}
+        configureButtons();
 
-	private class SortOrderChangeListener implements ChangeListener<WordRecord.SortOrder> {
-		@Override
-		public void changed(ObservableValue<? extends SortOrder> observable, SortOrder oldValue, SortOrder newValue) {
-			setSortOrder(newValue);
-		}
-	}
+        // Sort list alphabetically.
+        SortedList<WordRecord> sortedList = new SortedList<>(wordRecordList);
+        sortedList.setComparator((wr1, wr2) -> wr1.getWord().compareToIgnoreCase(wr2.getWord()));
+        listView.setItems(sortedList);
 
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		// Initialize the list.
-		SampleData.fillSampleData(wordRecordList);
+        addListeners();
 
-		configureButtons();
-		populateChoiceBox();
+        // Pre-select the first item.
+        listView.getSelectionModel().selectFirst();
+    }
 
-		// Sort list alphabetically.
+    private void configureButtons() {
+        // Disable the Remove button if nothing is selected in the ListView control.
+        removeButton.disableProperty()
+                .bind(listView.getSelectionModel().selectedItemProperty().isNull());
 
-		addListeners();
+        // Disable the Update button if nothing is selected, no modifications have
+        // been made, or any field is empty or invalid.
+        updateButton.disableProperty()
+                .bind(listView.getSelectionModel().selectedItemProperty().isNull()
+                        .or(modifiedProperty.not())
+                        .or(wordTextField.textProperty().isEmpty())
+                        .or(definitionTextArea.textProperty().isEmpty()));
 
-		// Pre-select the first item.
-		listView.getSelectionModel().selectFirst();
-	}
+        // TODO: Disable the Create button if an existing entry is selected or any
+        // field is empty or invalid.
+    }
 
-	private void configureButtons() {
-		// Disable the Remove button if nothing is selected in the ListView control.
-		removeButton.disableProperty().bind(listView.getSelectionModel().selectedItemProperty().isNull());
 
-		// Disable the Update button if nothing is selected, no modifications have
-		// been made, or any field is empty or invalid.
-		updateButton.disableProperty()
-				.bind(listView.getSelectionModel().selectedItemProperty().isNull().or(modifiedProperty.not())
-						.or(wordTextField.textProperty().isEmpty()).or(definitionTextArea.textProperty().isEmpty()));
+    private void addListeners() {
+        listView.getSelectionModel().selectedItemProperty().addListener(wordRecordChangeListener);
+    }
 
-		// Disable the Create button if an existing entry is selected or any
-		// field is empty or invalid.
-		createButton.disableProperty().bind(listView.getSelectionModel().selectedItemProperty().isNotNull()
-				.or(wordTextField.textProperty().isEmpty()).or(definitionTextArea.textProperty().isEmpty()));
-	}
+    @FXML
+    private void handleKeyAction(KeyEvent keyEvent) {
+        modifiedProperty.set(true);
+    }
 
-	private void populateChoiceBox() {
-		// https://stackoverflow.com/a/27801365/631051
-		sortChoiceBox.setItems(FXCollections.observableArrayList(WordRecord.SortOrder.values()));
-		sortChoiceBox.setValue(WordRecord.SortOrder.ALPHABETICALLY_FORWARD);
-		setSortOrder(WordRecord.SortOrder.ALPHABETICALLY_FORWARD);
-	}
+    @FXML
+    private void createButtonAction(ActionEvent actionEvent) {
+        System.out.println("Create");
+        WordRecord wordRecord =
+                new WordRecord(wordTextField.getText(), definitionTextArea.getText());
+        wordRecordList.add(wordRecord);
+        listView.getSelectionModel().select(wordRecord); // select the new item
+    }
 
-	private void addListeners() {
-		// Listen for records to be selected.
-		listView.getSelectionModel().selectedItemProperty().addListener(wordRecordChangeListener);
+    @FXML
+    private void removeButtonAction(ActionEvent actionEvent) {
+        System.out.println("Remove " + selectedWordRecord);
+        wordRecordList.remove(selectedWordRecord);
+    }
 
-		sortChoiceBox.getSelectionModel().selectedItemProperty().addListener(new SortOrderChangeListener());
-
-	}
-
-	@FXML
-	private void handleKeyAction(KeyEvent keyEvent) {
-		modifiedProperty.set(true);
-	}
-
-	@FXML
-	private void createButtonAction(ActionEvent actionEvent) {
-		System.out.println("Create");
-		WordRecord wordRecord = new WordRecord(wordTextField.getText(), Integer.parseInt(frequencyTextField.getText()),
-				definitionTextArea.getText());
-		wordRecordList.add(wordRecord);
-		// Select the new item.
-		listView.getSelectionModel().select(wordRecord);
-	}
-
-	@FXML
-	private void removeButtonAction(ActionEvent actionEvent) {
-		System.out.println("Remove " + selectedWordRecord);
-		wordRecordList.remove(selectedWordRecord);
-	}
-
-	@FXML
-	private void updateButtonAction(ActionEvent actionEvent) {
-		System.out.println("Update " + selectedWordRecord);
-		WordRecord entry = listView.getSelectionModel().getSelectedItem();
-		listView.getSelectionModel().selectedItemProperty().removeListener(wordRecordChangeListener);
-		entry.setWord(wordTextField.getText());
-		entry.setFrequency(Integer.parseInt(frequencyTextField.getText()));
-		entry.setDefinition(definitionTextArea.getText());
-		listView.getSelectionModel().selectedItemProperty().addListener(wordRecordChangeListener);
-		modifiedProperty.set(false);
-	}
+    @FXML
+    private void updateButtonAction(ActionEvent actionEvent) {
+        System.out.println("Update " + selectedWordRecord);
+        WordRecord entry = listView.getSelectionModel().getSelectedItem();
+        listView.getSelectionModel().selectedItemProperty()
+                .removeListener(wordRecordChangeListener);
+        entry.setWord(wordTextField.getText());
+        entry.setDefinition(definitionTextArea.getText());
+        listView.getSelectionModel().selectedItemProperty().addListener(wordRecordChangeListener);
+        modifiedProperty.set(false);
+    }
 }
--- src/main/java/edu/mills/cs180a/wordui/model/WordRecord.java
+++ src/main/java/edu/mills/cs180a/wordui/model/WordRecord.java
@@ -1,117 +1,62 @@
 package edu.mills.cs180a.wordui.model;
 
-import java.util.Comparator;
 import java.util.Objects;
-
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class WordRecord {
-	private static Comparator<WordRecord> azComparator = ((w1, w2) -> w1.getWord().compareToIgnoreCase(w2.getWord()));
-	private static Comparator<WordRecord> zaComparator = ((w1, w2) -> w2.getWord().compareToIgnoreCase(w1.getWord()));
-	private static Comparator<WordRecord> increasingFreqComparator = ((w1, w2) -> w1.getFrequency()
-			.compareTo(w2.getFrequency()));
-	private static Comparator<WordRecord> decreasingFreqComparator = ((w1, w2) -> w2.getFrequency()
-			.compareTo(w1.getFrequency()));
+    private final StringProperty word = new SimpleStringProperty(this, "word", "");
+    private final StringProperty definition =
+            new SimpleStringProperty(this, "definition", "sample definition");
 
-	public enum SortOrder {
-		ALPHABETICALLY_FORWARD("alphabetically (A-Z)", azComparator),
-		ALPHABETICALLY_BACKWARD("alphabetically (Z-A)", zaComparator),
-		BY_FREQ_ASCENDING("by frequency (low to high)", increasingFreqComparator),
-		BY_FREQ_DESCENDING("by frequency (high to low)", decreasingFreqComparator);
+    public WordRecord() {}
 
-		private final String name;
-		private final Comparator<WordRecord> comparator;
+    public WordRecord(String word, String definition) {
+        this.word.set(word);
+        this.definition.set(definition);
+    }
 
-		private SortOrder(String name, Comparator<WordRecord> comparator) {
-			this.name = name;
-			this.comparator = comparator;
-		}
+    public String getWord() {
+        return word.get();
+    }
 
-		@Override
-		public String toString() {
-			return name;
-		}
+    public StringProperty wordProperty() {
+        return word;
+    }
 
-		public Comparator<WordRecord> getComparator() {
-			return comparator;
-		}
-	}
+    public void setWord(String word) {
+        this.word.set(word);
+    }
 
-	private final StringProperty word = new SimpleStringProperty(this, "word", "");
-	private final IntegerProperty frequency = new SimpleIntegerProperty(this, "frequency");
-	private final StringProperty definition = new SimpleStringProperty(this, "definition", "sample definition");
+    public String getDefinition() {
+        return definition.get();
+    }
 
-	public WordRecord() {
-	}
+    public StringProperty definitionProperty() {
+        return definition;
+    }
 
-	public WordRecord(String word, int frequency, String definition) {
-		this.word.set(word);
-		this.frequency.set(frequency);
-		this.definition.set(definition);
-	}
+    public void setDefinition(String definition) {
+        this.definition.set(definition);
+    }
 
-	public String getWord() {
-		return word.get();
-	}
+    @Override
+    public String toString() {
+        return word.get();
+    }
 
-	public StringProperty wordProperty() {
-		return word;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        WordRecord record = (WordRecord) obj;
+        return Objects.equals(word, record.word) && Objects.equals(definition, record.definition);
+    }
 
-	public void setWord(String word) {
-		this.word.set(word);
-	}
-
-	public Integer getFrequency() {
-		return frequency.get();
-	}
-
-	public IntegerProperty frequencyProperty() {
-		return frequency;
-	}
-
-	public void setFrequency(int frequency) {
-		this.frequency.set(frequency);
-	}
-
-	public String getDefinition() {
-		return definition.get();
-	}
-
-	public StringProperty definitionProperty() {
-		return definition;
-	}
-
-	public void setDefinition(String definition) {
-		this.definition.set(definition);
-	}
-
-	@Override
-	public String toString() {
-		return String.format("%s (%d)", word.get(), frequency.get());
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null || getClass() != obj.getClass())
-			return false;
-		WordRecord record = (WordRecord) obj;
-		return Objects.equals(word, record.word) && Objects.equals(frequency, record.frequency)
-				&& Objects.equals(definition, record.definition);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(word, frequency, definition);
-	}
-
-	/*
-	 * public static Callback<Person, Observable[]> extractor = p -> new
-	 * Observable[] {p.lastnameProperty(), p.firstnameProperty()};
-	 */
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, definition);
+    }
 }
