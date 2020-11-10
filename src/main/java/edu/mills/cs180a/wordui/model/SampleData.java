af51854d93c01729b0163631baceaf51f6d9e594
Ellen Spertus <spertus@mills.edu>
2020-11-10 13:09:00 -0800

    Add word of the day to sample data

--- src/main/java/edu/mills/cs180a/wordui/model/SampleData.java
+++ src/main/java/edu/mills/cs180a/wordui/model/SampleData.java
@@ -1,9 +1,27 @@
 package edu.mills.cs180a.wordui.model;
 
+import java.io.IOException;
+import java.util.List;
+import edu.mills.cs180a.wordnik.client.ApiClientHelper;
+import edu.mills.cs180a.wordnik.client.api.WordsApi;
+import edu.mills.cs180a.wordnik.client.invoker.ApiClient;
+import edu.mills.cs180a.wordnik.client.model.WordOfTheDay;
 import javafx.collections.ObservableList;
 
 public class SampleData {
     public static void fillSampleData(ObservableList<WordRecord> backingList) {
+        try {
+            ApiClient client = ApiClientHelper.getApiClient();
+            WordsApi wordsApi = client.buildClient(WordsApi.class);
+            WordOfTheDay word = wordsApi.getWordOfTheDay();
+            List<Object> definitions = word.getDefinitions();
+            String definition = definitions.isEmpty() ? "none" : definitions.get(0).toString();
+            backingList.add(
+                    new WordRecord(word.getWord(), 0, definition));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
         backingList.add(new WordRecord("buffalo", 5153, "The North American bison."));
         backingList.add(new WordRecord("school", 23736, "A large group of aquatic animals."));
         backingList.add(new WordRecord("Java",
