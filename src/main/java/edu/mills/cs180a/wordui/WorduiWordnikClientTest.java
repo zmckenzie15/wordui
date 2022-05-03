1f858643e130ba839c2bc2fc6d199791d343e6d4
Ellen Spertus <spertus@mills.edu>
2022-05-03 14:56:28 -0700

    Fix mocking of orange frequencies

--- src/main/java/edu/mills/cs180a/wordui/WorduiWordnikClientTest.java
+++ src/main/java/edu/mills/cs180a/wordui/WorduiWordnikClientTest.java
@@ -15,6 +15,7 @@ class WorduiWordnikClientTest {
     private static WorduiWordnikClient client;
     private static WordApi mockWordApi = mock(WordApi.class);
     private static FrequencySummary mockFrequencySummaryApple = mock(FrequencySummary.class);
+    private static FrequencySummary mockFrequencySummaryOrange = mock(FrequencySummary.class);
     // These should be returned by getFrequency() for "apple" and "orange"
     private static final List<Object> APPLE_FREQUENCIES =
             List.of(makeMap(2000, 339), makeMap(2001, 464), makeMap(2020, 0));
@@ -30,7 +31,10 @@ class WorduiWordnikClientTest {
     public static void setup() throws IOException {
         when(mockWordApi.getWordFrequency(eq("apple"), anyString(), anyInt(), anyInt()))
                 .thenReturn(mockFrequencySummaryApple);
+        when(mockWordApi.getWordFrequency(eq("orange"), anyString(), anyInt(), anyInt()))
+                .thenReturn(mockFrequencySummaryOrange);
         when(mockFrequencySummaryApple.getFrequency()).thenReturn(APPLE_FREQUENCIES);
+        when(mockFrequencySummaryOrange.getFrequency()).thenReturn(ORANGE_FREQUENCIES);
         client = WorduiWordnikClient.getMockInstance(mockWordApi);
     }
 
